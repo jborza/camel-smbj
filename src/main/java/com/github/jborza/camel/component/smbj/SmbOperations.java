d111318969df66b83327ddaf1a3a54f97dfeb338
jborza <jborza@gmail.com>
2018-04-10 08:21:20 +0200

    introduced support for localWorkDirectory parameter as in (camel-jcifs and camel-ftp)

--- src/main/java/com/github/jborza/camel/component/smbj/SmbOperations.java
+++ src/main/java/com/github/jborza/camel/component/smbj/SmbOperations.java
@@ -3,13 +3,11 @@ package com.github.jborza.camel.component.smbj;
 import com.hierynomus.smbj.SMBClient;
 import org.apache.camel.Exchange;
 import org.apache.camel.component.file.*;
+import org.apache.camel.util.FileUtil;
 import org.apache.camel.util.IOHelper;
 import org.apache.camel.util.ObjectHelper;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.List;
 
 public class SmbOperations implements GenericFileOperations<SmbFile>, SmbShareFactory {
@@ -68,6 +66,92 @@ public class SmbOperations implements GenericFileOperations<SmbFile>, SmbShareFa
 
     @Override
     public boolean retrieveFile(String name, Exchange exchange) throws GenericFileOperationFailedException {
+        if (ObjectHelper.isNotEmpty(endpoint.getLocalWorkDirectory())) {
+            // local work directory is configured so we should store file content as files in this local directory
+            return retrieveFileToFileInLocalWorkDirectory(name, exchange);
+        } else {
+            // store file content directory as stream on the body
+            return retrieveFileToStreamInBody(name, exchange);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean retrieveFileToFileInLocalWorkDirectory(String name, Exchange exchange) throws GenericFileOperationFailedException {
+        File temp;
+
+        File local = new File(endpoint.getLocalWorkDirectory());
+        local.mkdirs();
+        OutputStream os;
+        GenericFile<SmbFile> file = (GenericFile<SmbFile>) exchange.getProperty(FileComponent.FILE_EXCHANGE_FILE);
+        ObjectHelper.notNull(file, "Exchange should have the " + FileComponent.FILE_EXCHANGE_FILE + " set");
+
+        // use relative filename in local work directory
+        String relativeName = file.getRelativeFilePath();
+        temp = new File(local, relativeName + ".inprogress");
+        local = new File(local, relativeName);
+
+        // delete any existing files
+        cleanupLocalFile(temp);
+        cleanupLocalFile(local);
+
+        // create new temp local work file
+        createNewTempLocalFile(temp);
+
+        // output stream points to the temp file in the local work directory
+        try {
+            os = new FileOutputStream(temp);
+        } catch (FileNotFoundException e1) {
+            throw new GenericFileOperationFailedException("Local work file not found: " + temp, e1);
+        }
+
+        // set header with the path to the local work file
+        exchange.getIn().setHeader(Exchange.FILE_LOCAL_WORK_PATH, local.getPath());
+
+        try {
+            // store the File reference as the message body
+            file.setBody(local);
+            smbClient.retrieveFile(name, os);
+        } catch (IOException e) {
+            throw new GenericFileOperationFailedException("Cannot retrieve file: " + name, e);
+        } catch (Exception e) {
+            throw new GenericFileOperationFailedException("Cannot retrieve file: " + name, e);
+        } finally {
+            IOHelper.close(os, "retrieve: " + name);
+        }
+
+        renameTempToTargetFile(temp, local);
+
+        return true;
+    }
+
+    private void createNewTempLocalFile(File temp) {
+        try {
+            if (!temp.createNewFile()) {
+                throw new GenericFileOperationFailedException("Cannot create new local work file: " + temp);
+            }
+        } catch (IOException e1) {
+            throw new GenericFileOperationFailedException("Cannot create new local work file: " + temp, e1);
+        }
+    }
+
+    private void cleanupLocalFile(File file) {
+        if (file.exists() && (!FileUtil.deleteFile(file))) {
+            throw new GenericFileOperationFailedException("Cannot delete existing local work file: " + file);
+        }
+    }
+
+    private void renameTempToTargetFile(File temp, File local) {
+        try {
+            if (!FileUtil.renameFile(temp, local, true)) {
+                throw new GenericFileOperationFailedException("Cannot rename local work file from: " + temp + " to: " + local);
+            }
+        } catch (IOException e) {
+            throw new GenericFileOperationFailedException("Cannot rename local work file from: " + temp + " to: " + local, e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private boolean retrieveFileToStreamInBody(String name, Exchange exchange) throws GenericFileOperationFailedException {
         OutputStream os = new ByteArrayOutputStream();
         GenericFile<SmbFile> target = (GenericFile<SmbFile>) exchange.getProperty(FileComponent.FILE_EXCHANGE_FILE);
         ObjectHelper.notNull(target, "Exchange should have the " + FileComponent.FILE_EXCHANGE_FILE + " set");
